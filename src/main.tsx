a34b58d5fb276d728cf7b31fb77477576937a71a
drngovothiennhan <dr.ngovothiennhan@gmail.com>
2026-09-10 15:49:35 +0700

    ui(yquan): load scale harmony layer after V17

--- src/main.tsx
+++ src/main.tsx
@@ -41,6 +41,7 @@ import './yquan-v14-clinic-flow.css';
 import './yquan-v15-engagement.css';
 import './yquan-v16-character-content.css';
 import './yquan-v17-three-beds-flow.css';
+import './yquan-scale-harmony.css';
 
 bootstrapThemeState();
 
